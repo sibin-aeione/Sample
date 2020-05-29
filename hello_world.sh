86a98f7c6654d4477f83887adb6d7d99e68bdf77
sibin-aeione <65276475+sibin-aeione@users.noreply.github.com>
2020-05-29 18:58:26 +0530

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # declare STRING variable
-STRING="Hello World..Stay Home..Stay Safe..2020!"
+STRING="Hello World..Stay Home..Stay Safe..2020!!"
 #print variable on a screen
 echo $STRING
